82ec1d6c5d58e3482d5b322ed9d21bbfadd3e6dc
singhalbhaskar <singhalbhaskar@google.com>
2026-03-20 18:39:22 +0530

    Create remote_deploy.py
    Signed-off-by: singhalbhaskar <singhalbhaskar@google.com>

---
+++ python/agents/data-science/deployment/remote_deploy.py
@@ -0,0 +1,24 @@
+import os
+
+import vertexai
+from absl import app, flags
+from data_science.agent import root_agent
+from dotenv import load_dotenv
+from vertexai import agent_engines
+from vertexai.preview.reasoning_engines import AdkApp
+
+FLAGS = flags.FLAGS
+flags.DEFINE_string("project_id", None, "GCP project ID.")
+flags.DEFINE_string("location", None, "GCP location.")
+flags.DEFINE_string("bucket", None, "GCP bucket.")
+flags.DEFINE_string("resource_id", None, "ReasoningEngine resource ID.")
+
+flags.DEFINE_bool("list", False, "List all agents.")
+flags.DEFINE_bool("create", False, "Creates a new agent.")
+flags.DEFINE_bool("delete", False, "Deletes an existing agent.")
+flags.mark_bool_flags_as_mutual_exclusive(["create", "delete"])
+
+adk_app = AdkApp(
+    agent=root_agent,
+    enable_tracing=True,
+)
